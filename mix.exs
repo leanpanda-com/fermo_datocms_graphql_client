3ed0e43ab206780fe3ec8adc643844146a93ddb9
Joe Yates <joe.g.yates@gmail.com>
2021-05-13 15:44:49 +0200

    Remove Fermo dependency

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Fermo.DatoCMS.GraphqlClient.MixProject do
   use Mix.Project
 
-  @version "0.14.3"
+  @version "0.14.4"
   @git_origin "https://github.com/leanpanda-com/fermo_datocms_graphql_client"
 
   def project do
@@ -16,7 +16,7 @@ defmodule Fermo.DatoCMS.GraphqlClient.MixProject do
       docs: [
         extras: ["README.md"],
         homepage_url: @git_origin,
-        main: "Fermo",
+        main: "Fermo.DatoCMS.GraphQLClient",
         source_ref: "v#{@version}",
         source_url: @git_origin
       ]
@@ -42,8 +42,7 @@ defmodule Fermo.DatoCMS.GraphqlClient.MixProject do
   defp deps do
     [
       {:datocms_graphql_client, ">= 0.13.0"},
-      {:ex_doc, "~> 0.21.2", only: :dev},
-      {:fermo, ">= 0.13.0"}
+      {:ex_doc, "~> 0.21.2", only: :dev}
     ]
   end
 end
